d77627e33ad4799ddf3d3cc4ddfe16e2e13b81fd
Thewsthews <etiegnim@gmail.com>
2025-02-16 23:56:20 +0300

    Changes to wallet.rs

--- titanvault-backend/src/wallet.rs
+++ titanvault-backend/src/wallet.rs
@@ -7,3 +7,14 @@ pub struct WalletResponse{
     pub private_key: String,
     pub mnemonic: String,
 }
+
+pub fn generate_wallet() -> WalletResponse{
+    let mnemonic = MnemonicBuilder::default().word_count(12).generate().unwrap();
+    let wallet = mnemonic.clone().into_wallet().unwrap();
+
+    WalletResponse{
+        address: wallet.address().to_string(),
+        private_key: wallet.private_key().unwrap().to_string(),
+        mnemonic: mnemonic.phrase(),
+    }
+}
