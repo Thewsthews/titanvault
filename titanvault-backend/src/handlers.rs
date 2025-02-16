1bd5553f799f6519384bcb96fe82b595bd2f203a
Thewsthews <etiegnim@gmail.com>
2025-02-16 23:49:28 +0300

    Changes to handlers.rs

--- titanvault-backend/src/handlers.rs
+++ titanvault-backend/src/handlers.rs
@@ -12,4 +12,23 @@ struct WalletResponse{
 
 pub async fn create_wallet() -> Json<WalletResponse>{
     let wallet_data = wallet::generate_wallet();
+    Json(wallet_data)
+}
+
+#[derive(Deserialize)]
+struct SignRequest{
+    private_key: String,
+    transaction_data: String,
+}
+
+#[derive(Serialize)]
+
+struct SignResponse{
+    signed_transaction: String,
+}
+
+pub async fn sigh_transaction(Json(payload: Json<SignRequest>)-> Json<SignResponse>{
+    let transaction = Transaction::new(payload.private_key.clone(), payload.transaction_data.clone());
+    let signed_transaction = transaction.sign();
+    Json(SignResponse{signed_transaction})
 }
