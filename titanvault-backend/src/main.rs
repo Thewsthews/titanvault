12e49c0fe797fae677a0f06f0c4adc98075a0edf
Thewsthews <etiegnim@gmail.com>
2025-02-16 23:37:59 +0300

    Changes

--- titanvault-backend/src/main.rs
+++ titanvault-backend/src/main.rs
@@ -1,13 +1,12 @@
-use axum::{routing::post, Router, Json};
-use ethers::signers::{coins_bip39::mnemonic, LocalWallet, MnemonicBuilder};
-use serde::{Deserialize, Serialize};
+use axum::{routing::post, Router};
 use std::net::SocketAddr;
+use tokio;
+
+mod routes
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new()
-    .route("/create_wallet", post(create_wallet))
-    .route("/sign_transaction",post(sign_transaction));
+    let app= routes::create_router();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on http://{}", addr);
@@ -15,15 +14,4 @@ async fn main(){
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-#[derive(Serialize)]
-struct WalletResponse{
-    address: String,
-    private_key: String,
-    mnemonic: String,
-}
-async fn create_wallet() -> Json<WalletResponse>{
-    let mnemonic = MnemonicBuilder::default().word_count(12).build().unwrap();
-    let wallet = mnemonic.clone().build().unwrap();
 }
