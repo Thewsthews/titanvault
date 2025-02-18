f423c73b96806820d04103c69df9b450e727b0bc
Thewsthews <etiegnim@gmail.com>
2025-02-18 20:42:26 +0300

    Changes to the wallet.rs file

--- titanvault-backend/src/wallet.rs
+++ titanvault-backend/src/wallet.rs
@@ -1,38 +1,60 @@
-use ethers::{signers::{LocalWallet, MnemonicBuilder, Signer}, types::transaction::eip2718::TypedTransaction};
-use std::str::FromStr;
+use ethers::{
+    signers::{LocalWallet, MnemonicBuilder},
+    types::transaction::eip2718::TypedTransaction, utils::hex::hex,
+};
+use k256::{ecdsa::{signature::Signer as k256Signer, SigningKey, VerifyingKey}, elliptic_curve::rand_core};
+use rand_core::OsRng;
+use std::{str::FromStr, error::Error};
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct WalletResponse {
+pub struct WalletResponse{
     pub address: String,
     pub private_key: String,
     pub mnemonic: String,
 }
 
-pub fn generate_wallet() -> WalletResponse {
-    // Generate a mnemonic
+
+pub fn generate_wallet() -> WalletResponse{
+    //This is reponsible for generation of a mnemonic
     let mnemonic = MnemonicBuilder::default()
-        .word_count(12)
-        .build()
-        .unwrap();
+    .word_count(12)
+    .build()
+    .unwrap();
 
-    // Derive a wallet from the mnemonic
+    //Derives a wallet from the mnemonic
     let wallet = mnemonic.clone()
-        .generate()
-        .unwrap();
+    .build()
+    .unwrap();
 
-    WalletResponse {
+    WalletResponse{
         address: wallet.address().to_string(),
-        private_key: wallet.signer().to_string(),
-        mnemonic: mnemonic.to_string(),
+        private_key: wallet.private_key().unwrap().to_string(),
+        mnemonic: mnemonic.phrase(),
     }
 }
-
 pub async fn sign_transaction(
     private_key: &str,
     transaction: &TypedTransaction,
-) -> Result<String, Box<dyn std::error::Error>> {
+)-> Result<String, Box<dyn Error>>{
     let wallet = LocalWallet::from_str(private_key)?;
-    let signed_tx = wallet.sign_transaction(transaction).await?;
-    Ok(format!("{:?}", signed_tx))
+    let signature = wallet.sign(transaction).await?;
+    Ok(signature.to_string())
+}
+
+//This generates a new secp256k1 private key using the k256 protocol
+pub fn generate_k256_key()->(String, String){
+    let signing_key = SigningKey::random(&mut OsRng);
+    let verifying_key = VerifyingKey::from(&signing_key);
+    (
+        hex::encode(signing_key.to_bytes().as_ref()),
+        hex::encode(verifying_key.to_sec1_bytes().as_ref()),
+    )
+}
+
+///This is the part that signs a message using k256
+pub fn sign_message_k256(private_key: &str, message: &[u8]) -> Result<String, Box<dyn Error>>{
+    let signing_key = SigningKey::from_bytes(&hex::decode(private_key)?)?;
+    let signature = signing_key.sign(message);
+    Ok(hex::encode(signature.to_bytes().as_ref()))
 }
