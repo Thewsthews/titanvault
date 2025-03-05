77d5995051a5071cdc78382f04029ac6250a9d9e
Thewsthews <etiegnim@gmail.com>
2025-03-06 00:22:26 +0300

    Upgrades to the rust-crypto crate with a more up-to-date version.

---
+++ titanvault-backend/target/debug/deps/titanvault_backend-128a91e3d0ceae7f.d
@@ -0,0 +1,9 @@
+/home/m1/Desktop/CodeWork/titanvault/titanvault-backend/target/debug/deps/libtitanvault_backend-128a91e3d0ceae7f.rmeta: src/lib.rs src/routes.rs src/wallet.rs src/handlers.rs src/transactions.rs
+
+/home/m1/Desktop/CodeWork/titanvault/titanvault-backend/target/debug/deps/titanvault_backend-128a91e3d0ceae7f.d: src/lib.rs src/routes.rs src/wallet.rs src/handlers.rs src/transactions.rs
+
+src/lib.rs:
+src/routes.rs:
+src/wallet.rs:
+src/handlers.rs:
+src/transactions.rs:
